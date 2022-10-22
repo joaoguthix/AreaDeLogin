8c5b53c92a298399415a37ab20f14d116b1e4e62
joao vitor <guerreiroxx8@gmail.com>
2022-10-22 14:38:04 -0300

    teste

---
+++ MyTodoApp/Models/Todo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Titulo")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [DisplayName("Útima atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+        [DisplayName("Usuário")]
+        public string User { get; set; }
+    }
+}
